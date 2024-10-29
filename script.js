fe65da33681c123da559e718d9af8870ab04c1c5
Issac-Eligulashvili <issac.eli2007@gmail.com>
2024-10-28 22:10:00 -0400

    added local storage because i was bored. Absolutely not helpful in the slighest and you would have to press the button again if time or day changes but whatever not my problem 😂😂😂😂

--- script.js
+++ script.js
@@ -21,6 +21,10 @@ function getClassesForDay(data, day) {
      return data.schedule.filter(classInfo => classInfo.days.includes(day))
 }
 
+//setting the data using local storage cuz why not?
+let loadedData = localStorage.getItem('scheduleTable');
+$('#scheduleList').html(loadedData);
+
 function convertTime(time) {
      //check if it is AM/PM and store that value within a variable
      let meridiem = time.split(' ')[1];
@@ -44,72 +48,74 @@ function isCurrentClass(startTime, endTime) {
 }
 
 $('#submitDay').on('click', () => {
+     //clear all schedule data
+     $('#scheduleList').empty();
+
      //check if the schedule list is empty to prevent duplicates being added
-     if ($('#scheduleList').html() === '') {
-          $.ajax({
-               type: "GET",
-               url: "https://api.npoint.io/07f64e16e4f3361bde3e",
-               success: function (data) {
-                    //get the list of classes for that day using filter
-                    const classesForToday = getClassesForDay(data, currentDay);
-                    //create the order of the periods based on the letter day for organization and double lab
-                    const periodMap = {
-                         'A': [1, 1, 3, 5, 6],
-                         'B': [4, 1, 2, 7, 5],
-                         'C': [3, 4, 1, 6, 7],
-                         'D': [2, 3, 4, 5, 6],
-                         'E': [1, 2, 3, 7, 5],
-                         'F': [4, 1, 2, 6, 7],
-                         'G': [3, 4, 7, 5, 6],
+     $.ajax({
+          type: "GET",
+          url: "https://api.npoint.io/07f64e16e4f3361bde3e",
+          success: function (data) {
+               //get the list of classes for that day using filter
+               const classesForToday = getClassesForDay(data, currentDay);
+               //create the order of the periods based on the letter day for organization and double lab
+               const periodMap = {
+                    'A': [1, 1, 3, 5, 6],
+                    'B': [4, 1, 2, 7, 5],
+                    'C': [3, 4, 1, 6, 7],
+                    'D': [2, 3, 4, 5, 6],
+                    'E': [1, 2, 3, 7, 5],
+                    'F': [4, 1, 2, 6, 7],
+                    'G': [3, 4, 7, 5, 6],
 
-                    }
-                    //get the correct order for periods based on the current day's letter
-                    const periods = periodMap[currentDay];
-                    //create a new array which maps the correct class to the period based on the order of the periods
-                    const newArray = periods.map(block => classesForToday.find(obj => obj.period === block));
-                    //create the time for the bell schedule and inclue lunch times.
-                    let bellSchedule;
+               }
+               //get the correct order for periods based on the current day's letter
+               const periods = periodMap[currentDay];
+               //create a new array which maps the correct class to the period based on the order of the periods
+               const newArray = periods.map(block => classesForToday.find(obj => obj.period === block));
+               //create the time for the bell schedule and inclue lunch times.
+               let bellSchedule;
 
-                    if (isHalfDay) {
-                         bellSchedule = {
-                              1: { start: '8:24 AM', end: '9:14 AM' },
-                              2: { start: '9:19 AM', end: '10:09 AM' },
-                              3: { start: '10:14 AM', end: '11:04 AM' },
-                              4: { start: '11:09 AM', end: '11:59 AM' },
-                              5: { start: '12:04 PM', end: '12:54 PM' }
-                         }
-                    } else {
-                         bellSchedule = {
-                              1: { start: '8:24 AM', end: '9:31 AM' },
-                              2: { start: '9:36 AM', end: '10:43 AM' },
-                              3: { start: '10:48 AM', end: '11:55 AM' },
-                              lunch: { start: '12:00 PM', end: '12:35 PM' },
-                              4: { start: '12:41 PM', end: '1:48 PM' },
-                              5: { start: '1:53 PM', end: '3:00 PM' }
-                         }
+               if (isHalfDay) {
+                    bellSchedule = {
+                         1: { start: '8:24 AM', end: '9:14 AM' },
+                         2: { start: '9:19 AM', end: '10:09 AM' },
+                         3: { start: '10:14 AM', end: '11:04 AM' },
+                         4: { start: '11:09 AM', end: '11:59 AM' },
+                         5: { start: '12:04 PM', end: '12:54 PM' }
                     }
-                    newArray.forEach((item, index) => {
-                         item.time = bellSchedule[index + 1]; //set the correct time for that class based on the periods of the class, making sure the period and time for that period correspond
-                         //append the rows based on the current index
-                         $('#scheduleList').append(
-                              `<tr>
+               } else {
+                    bellSchedule = {
+                         1: { start: '8:24 AM', end: '9:31 AM' },
+                         2: { start: '9:36 AM', end: '10:43 AM' },
+                         3: { start: '10:48 AM', end: '11:55 AM' },
+                         lunch: { start: '12:00 PM', end: '12:35 PM' },
+                         4: { start: '12:41 PM', end: '1:48 PM' },
+                         5: { start: '1:53 PM', end: '3:00 PM' }
+                    }
+               }
+               newArray.forEach((item, index) => {
+                    item.time = bellSchedule[index + 1]; //set the correct time for that class based on the periods of the class, making sure the period and time for that period correspond
+                    //append the rows based on the current index
+                    $('#scheduleList').append(
+                         `<tr>
                                    <td>${index + 1}</td>
                                    <td>${item.time.start} - ${item.time.end}</td>
                                    <td>${item.class}</td>
                                    <td>${item.teacher}</td>
                                    <td>${item.room}</td>
                               </tr>`
-                         )
+                    )
 
-                         if (isCurrentClass(item.time.start, item.time.end)) {
-                              $('#scheduleList').last().css({
-                                   backgroundColor: 'yellow',
-                              })
-                         }
-                         //create the lunch item after the 3rd row was created but before the 4th row is 
-                         if (index === 2 && !isHalfDay) {
-                              $('#scheduleList').append(
-                                   `<tr>
+                    if (isCurrentClass(item.time.start, item.time.end)) {
+                         $('#scheduleList').last().css({
+                              backgroundColor: 'yellow',
+                         })
+                    }
+                    //create the lunch item after the 3rd row was created but before the 4th row is 
+                    if (index === 2 && !isHalfDay) {
+                         $('#scheduleList').append(
+                              `<tr>
                                         <td>LUNCH</td>
                                         <td>${bellSchedule.lunch.start} - ${bellSchedule.lunch.end}</td>
                                         <td>
@@ -118,13 +124,13 @@ $('#submitDay').on('click', () => {
                                         <td>N/A</td>
                                         <td>N/A</td>
                                    </tr>`
-                              )
-                         }
-                    })
-               }
-          });
-     } else {
-          alert('You already see your schedule!')
-     }
+                         )
+                    }
+               })
 
+               let scheduleData = $('#scheduleList').html();
+               console.log(scheduleData);
+               localStorage.setItem('scheduleTable', scheduleData);
+          }
+     });
 })
